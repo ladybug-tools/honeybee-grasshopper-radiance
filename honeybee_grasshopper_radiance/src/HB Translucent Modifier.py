8cb0463070be26a5f74c86c31541e48f9940428c
Chris Mackey <chris@ladybug.tools>
2021-12-31 21:24:45 -0500

    fix(modifiers): Make inputs for translucent modifier more intuitive
    I've come to realize that I have been using the Trans material wrong for all of these years because the properties in Radiance are not what they initially seem. And they are definitely not what you would get from any product manufacturer. So I am changing the component so that we can actually construct the material from transmittance and reflectance.

--- honeybee_grasshopper_radiance/src/HB Translucent Modifier.py
+++ honeybee_grasshopper_radiance/src/HB Translucent Modifier.py
@@ -8,22 +8,27 @@
 # @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
 """
-Create a translucent radiance modifier from a reflectances and transmittances.
+Create a translucent radiance modifier from a reflectance and transmittance.
+_
+The sum of the average reflectance and average transmittance must be less than 1
+and any energy not transmitted or reflected is assumed to be absorbed.
+The resulting material will always be grey with equivalent red, green and
+blue channels.
+_
+More properties are available with the "HB Translucent Modifier 3" component.
 -
 
     Args:
         _name_: Text to set the name for the modifier and to be incorporated into
             a unique modifier identifier.
-        _diff_ref: A number between 0 and 1 for the opaque diffuse reflectance.
-            This reflectance will be the same for the red, green and blue channels.
-        _diff_trans: A number between 0 and 1 for the transmitted diffuse component.
-            This is the fraction of transmitted light that is diffusely scattered.
-        _spec_trans: A number between 0 and 1 for the transmitted specular component.
-            This is the fraction of transmitted light that is not diffusely
-            scattered but passes through like a beam.
-        _spec_: A number between 0 and 1 for the fraction of specularity. Specularity
-            fractions greater than 0.1 are not common in non-metallic
-            materials. (Default: 0).
+        _avg_ref: The average reflectance of the material. The value should be
+            between 0 and 1.
+        _avg_trans: The average transmittance of the material. The value should be
+            between 0 and 1.
+        _is_spec_: Boolean to note if the reflected component is specular (True) or
+            diffuse (False). (Default: False).
+        _is_diffusing_: Boolean to note if the tranmitted component is diffused (True)
+            instead of specular like glass (False). (Default: True).
         _rough_: Roughness is specified as the rms slope of surface facets. A value
             of 0 corresponds to a perfectly smooth surface, and a value of 1 would be
             a very rough surface. Roughness values greater than 0.2 are not very
@@ -36,7 +41,7 @@ Create a translucent radiance modifier from a reflectances and transmittances.
 
 ghenv.Component.Name = 'HB Translucent Modifier'
 ghenv.Component.NickName = 'TransMod'
-ghenv.Component.Message = '1.3.0'
+ghenv.Component.Message = '1.3.1'
 ghenv.Component.Category = 'HB-Radiance'
 ghenv.Component.SubCategory = '1 :: Modifiers'
 ghenv.Component.AdditionalHelpFromDocStrings = '2'
@@ -59,13 +64,14 @@ except ImportError as e:
 
 if all_required_inputs(ghenv.Component):
     # set the default modifier properties
-    _spec_ = 0.0 if _spec_ is None else _spec_
+    _is_spec_ = False if _is_spec_ is None else _is_spec_
+    _is_diffusing_ = True if _is_diffusing_ is None else _is_diffusing_
     _rough_ = 0.0 if _rough_ is None else _rough_
     name = clean_and_id_rad_string('TransMaterial') if _name_ is None else \
         clean_rad_string(_name_)
 
     # create the modifier
-    modifier = Trans.from_single_reflectance(name, _diff_ref, _spec_, _rough_,
-                                             _diff_trans, _spec_trans)
+    modifier = Trans.from_average_properties(
+        name, _avg_ref, _avg_trans, _is_spec_, _is_diffusing_, _rough_)
     if _name_ is not None:
         modifier.display_name = _name_
