a04999c20eb67133931f2250f18d6d3aa8d4d0e4
Chris Mackey <chris@ladybug.tools>
2022-01-06 14:51:32 -0500

    fix(modifiers): Better fix for Trans modifier inputs

--- honeybee_grasshopper_radiance/src/HB Translucent Modifier 3.py
+++ honeybee_grasshopper_radiance/src/HB Translucent Modifier 3.py
@@ -8,7 +8,10 @@
 # @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
 
 """
-Create a translucent radiance modifier from a reflectances and transmittances.
+Create a translucent radiance modifier from reflectances and transmittances.
+_
+The sum of the reflectances and transmittances must be less than 1
+and any energy not transmitted or reflected is assumed to be absorbed.
 -
 
     Args:
@@ -19,9 +22,9 @@ Create a translucent radiance modifier from a reflectances and transmittances.
         _b_diff_ref: A number between 0 and 1 for the blue diffuse reflectance.
         _diff_trans: A number between 0 and 1 for the transmitted diffuse component.
             This is the fraction of transmitted light that is diffusely scattered.
-        _spec_trans: A number between 0 and 1 for the transmitted specular component.
+        _spec_trans_: A number between 0 and 1 for the transmitted specular component.
             This is the fraction of transmitted light that is not diffusely
-            scattered but passes through like a beam.
+            scattered but passes through like a beam. (Default: 0).
         _spec_: A number between 0 and 1 for the fraction of specularity. Specularity
             fractions greater than 0.1 are not common in non-metallic
             materials. (Default: 0).
@@ -37,7 +40,7 @@ Create a translucent radiance modifier from a reflectances and transmittances.
 
 ghenv.Component.Name = 'HB Translucent Modifier 3'
 ghenv.Component.NickName = 'TransMod3'
-ghenv.Component.Message = '1.3.0'
+ghenv.Component.Message = '1.3.1'
 ghenv.Component.Category = 'HB-Radiance'
 ghenv.Component.SubCategory = '1 :: Modifiers'
 ghenv.Component.AdditionalHelpFromDocStrings = '0'
@@ -60,13 +63,15 @@ except ImportError as e:
 
 if all_required_inputs(ghenv.Component):
     # set the default modifier properties
+    _spec_trans_ = 0.0 if _spec_trans_ is None else _spec_trans_
     _spec_ = 0.0 if _spec_ is None else _spec_
     _rough_ = 0.0 if _rough_ is None else _rough_
     name = clean_and_id_rad_string('TransMaterial') if _name_ is None else \
         clean_rad_string(_name_)
 
     # create the modifier
-    modifier = Trans(name, _r_diff_ref, _g_diff_ref, _b_diff_ref,
-                     _spec_, _rough_, _diff_trans, _spec_trans)
+    modifier = Trans.from_reflected_specularity(
+        name, _r_diff_ref, _g_diff_ref, _b_diff_ref,
+        _spec_, _rough_, _diff_trans, _spec_trans_)
     if _name_ is not None:
         modifier.display_name = _name_
