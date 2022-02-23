43235b041ec168798cc05cc13521a0d77e130da3
Chris Mackey <chris@ladybug.tools>
2022-02-22 21:53:13 -0500

    fix(result): Update docstrings and consolidate code

--- honeybee_grasshopper_radiance/src/HB Daylight Control Schedule.py
+++ honeybee_grasshopper_radiance/src/HB Daylight Control Schedule.py
@@ -17,13 +17,13 @@ In addition to benefiting from the accuracy of Radiance, using this component ha
 several advantages over the "HB Apply Daylight Control" component under HB-Energy.
 Notably, it can account for setups with multiple illuminance sensors.
 _
-This component expects the annual daylight simulation to be run with one sensor
-grid per room in the model. If the sensor grids within the annual daylight
-simulation are distrbuted over the entire floor of each room, the resulting
-schedules will be idealized, where light dimming has been optimized to supply
-the minimum illuminance setpoint everywhere in the room. Grids with one, two,
-or more sensors can be used to model setups where fractions of each room are
-controlled by different sensors.
+This component will generate one schedule per sensor grid in the simulation. Each
+grid should have sensors at the locations in space where daylight dimming sensors
+are located. Grids with one, two, or more sensors can be used to model setups
+where fractions of each room are controlled by different sensors. If the sensor
+grids are distributed over the entire floor of the rooms, the resulting schedules
+will be idealized, where light dimming has been optimized to supply the minimum
+illuminance setpoint everywhere in the room.
 -
 
     Args:
@@ -55,7 +55,7 @@ controlled by different sensors.
             system can dim down to, expressed as a fraction of maximum light
             output. Note that setting this to 1 means lights aren't dimmed at
             all until the illuminance setpoint is reached. This can be used to
-            approximate manual light-switching behaviour when used in conjunction
+            approximate manual light-switching behavior when used in conjunction
             with the off_at_min_ output below. (Default: 0.2).
         off_at_min_: Boolean to note whether lights should switch off completely when
             they get to the minimum power input. (Default: False).
@@ -67,7 +67,7 @@ controlled by different sensors.
 
 ghenv.Component.Name = 'HB Daylight Control Schedule'
 ghenv.Component.NickName = 'DaylightSchedule'
-ghenv.Component.Message = '1.4.0'
+ghenv.Component.Message = '1.4.1'
 ghenv.Component.Category = 'HB-Radiance'
 ghenv.Component.SubCategory = '4 :: Results'
 ghenv.Component.AdditionalHelpFromDocStrings = '1'
@@ -80,8 +80,7 @@ except ImportError as e:
     raise ImportError('\nFailed to import ladybug:\n\t{}'.format(e))
 
 try:
-    from honeybee_radiance.postprocess.annualdaylight import \
-        generate_default_schedule, _process_input_folder
+    from honeybee_radiance.postprocess.electriclight import daylight_control_schedules
 except ImportError as e:
     raise ImportError('\nFailed to import honeybee_radiance:\n\t{}'.format(e))
 
@@ -98,36 +97,6 @@ except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
 
 
-def diminng_from_ill(ill_val, ill_setpt, min_pow, min_light, off_at_min):
-    """Compute the dimming fraction from an illuminance value."""
-    if ill_val > ill_setpt:  # dimmed all of the way
-        return 0 if off_at_min else min_pow
-    elif ill_val <= min_light:  # not dimmed at all
-        return 1
-    else:  # partially dimmed
-        fract_dim = (ill_setpt - ill_val) / (ill_setpt - min_light)
-        return fract_dim + ((1 - fract_dim) * min_pow)
-
-
-def file_to_dimming_fraction(ill_file, su_pattern, setpt, m_pow, m_lgt, off_m):
-    """Compute hourly dimming fractions for a given result file."""
-    # get a base schedule of dimming fractions for the sun-up hours
-    su_values = [0] * len(su_pattern)
-    sensor_count = 0
-    with open(ill_file) as results:
-        for pt_res in results:
-            sensor_count += 1
-            for i, val in enumerate(pt_res.split()):
-                su_values[i] += diminng_from_ill(float(val), setpt, m_pow, m_lgt, off_m)
-    su_values = [val / sensor_count for val in su_values]
-
-    # account for the hours where the sun is not up
-    dim_fract = [1] * 8760
-    for val, hr in zip(su_values, su_pattern):
-        dim_fract[hr] = float(val)
-    return dim_fract
-
-
 if all_required_inputs(ghenv.Component):
     # set default values for all controls
     _ill_setpoint_ = 300 if _ill_setpoint_ is None else _ill_setpoint_
@@ -137,7 +106,7 @@ if all_required_inputs(ghenv.Component):
 
     # process the base schedule input into a list of values
     if _base_schedule_ is None:
-        schedule = generate_default_schedule()
+        schedule = _base_schedule_
     elif isinstance(_base_schedule_, BaseCollection):
         schedule = _base_schedule_.values
     elif isinstance(_base_schedule_, str):
@@ -151,23 +120,11 @@ if all_required_inputs(ghenv.Component):
     # get the relevant .ill files
     res_folder = os.path.dirname(_results[0]) if os.path.isfile(_results[0]) \
         else _results[0]
-    grids, sun_up_hours = _process_input_folder(res_folder, '*')
-    sun_up_hours = [int(h) for h in sun_up_hours]
-
-    # get the dimming fractions for each sensor grid from the .ill files
-    dim_fracts = []
-    for grid_info in grids:
-        ill_file = os.path.join(res_folder, '%s.ill' % grid_info['full_id'])
-        fract_list = file_to_dimming_fraction(
-            ill_file, sun_up_hours, _ill_setpoint_, _min_power_in_,
-            _min_light_out_, off_at_min_
-        )
-        dim_fracts.append(fract_list)
+    sch_vals, sch_ids = daylight_control_schedules(
+        res_folder, schedule, _ill_setpoint_, _min_power_in_, _min_light_out_, off_at_min_)
 
     # create the schedule by combining the base schedule with the dimming fraction
     type_limit = schedule_type_limit_by_identifier('Fractional')
     schedules = []
-    for grid_info, dim_fract in zip(grids, dim_fracts):
-        sch_vals = [b_val * d_val for b_val, d_val in zip(schedule, dim_fract)]
-        sch_id = '{} Daylight Control'.format(grid_info['full_id'])
-        schedules.append(ScheduleFixedInterval(sch_id, sch_vals, type_limit))
+    for shc_val, sch_id in zip(sch_vals, sch_ids):
+        schedules.append(ScheduleFixedInterval(sch_id, shc_val, type_limit))
